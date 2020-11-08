78c00560e5848418754b1f9fdb088fe9194350ec
ABAI DANIEL ADEWUNMI <51218816+DariesMedia@users.noreply.github.com>
2020-11-09 00:29:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ const m25 = '<span>&#36;</span>';
 
 // Sounds Object
 const funke = document.getElementById("funke");
-funke.volume = 0.2;
+funke.volume = 0.3;
 const punch = document.getElementById("punch");
 punch.volume = 0.1;
 const apostle = document.getElementById("apostle");
@@ -40,9 +40,17 @@ const keepQuiet = document.getElementById("keepQuiet");
 const ayele = document.getElementById("ayele");
 const clapping = document.getElementById("clapping");
 const chai = document.getElementById("chai");
+const correct = document.getElementById("correct");
 
 // Game Images In a List
 const imgList = [m1,m2, m3, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20, m21, m22, m23, m24];
+// Game Numbers In a List
+const numList = [];
+// GENERATING 15 NUMBERS BETWEEN 1 - 100 AND ADDING THEM TO A LIST (NUMLIST)
+for (var i=0; i<101;i++){
+  randomNumber = i;
+  numList.push(randomNumber);
+}
 
 // List Reorder Function
 const shuffleList = (array) => {
@@ -51,11 +59,14 @@ const shuffleList = (array) => {
 
 // Shuffling List
 shuffleList(imgList);
+shuffleList(numList);
+// Game Image and Number List
+const gameList = [numList, imgList];
 
 // Game Board Object
 const gBoard = document.getElementById('g-board');
 const gBoardCounter = document.getElementById('counter');
-const gBoardCoins = document.getElementById("coins");
+const gBoardScore = document.getElementById("score");
 const ant = document.getElementById("ant");
 const wrong = document.getElementById("wrong");
 const gamePlay = document.getElementById('gamePlay');
@@ -69,34 +80,61 @@ const gImage = document.getElementById("gImage");
 
 // Game Object Variables
 const gameCounter = 30;
+var userScore = 0;
 var userCoins = 0;
 var level = 1;
 var reshuffleMe = 0;
+// Select Which Board (Images or Numbers)
+var gamePick = gameList[Math.floor(Math.random() * gameList.length)];
 
 // Game Function
 const gShuffle = () => { 
   // Photos List
   const photos = [];
-  // Selecting the first 11 Images From imgList
-  imgList.forEach (function(item){
-    if (photos.length < 11) {
-      photos.push(item);
+  // Condition to Select Which Board
+  if (gamePick == gameList[1]){
+    // Selecting the first 11 Images From imgList
+    imgList.forEach (function(item){
+      if (photos.length < 11) {
+        photos.push(item);
+      }
+    });
+    // Picking Only One Image as Answer from imgList and Adding it 4 times to photos List
+    while (photos.length < 15) {
+      photos.push(imgList[12] + ' id ="ant" Onclick="antClick(this)" ');
     }
-  });
-  // Picking Only One Image as Answer from imgList and Adding it 4 times to photos List
-  while (photos.length < 15) {
-    photos.push(imgList[12] + ' id ="ant" Onclick="antClick(this)" ');
+    // Re-arranging Photos For Game Start
+    shuffleList(photos);
+    shuffleList(photos);
+    shuffleList(photos);
+    shuffleList(photos);
+    // Adding Game Photo to Game Board
+    photos.forEach(function(photo) {
+      const gPhoto = photo + 'id="wrong" onClick="wrongAnt()" ' + ' />';
+      gBoard.appendChild(document.createElement('div')).innerHTML = gPhoto;
+    });
+  }else {
+    // Selecting the first 14 From numList
+    numList.forEach (function(item){
+      if (photos.length < 14) {
+        photos.push('<div id="wrong" onClick="wrongAnt()"><br/>' + item + '</div>');
+      }
+    });
+    // Picking Only One Number as Answer from numList and Adding it 4 times to photos List
+    while (photos.length < 15) {
+      photos.push('<div id ="ant" Onclick="antClick(this)"><br/>' + numList[14] + '</div>');
+    }
+    // Re-arranging Photos For Game Start
+    shuffleList(photos);
+    shuffleList(photos);
+    shuffleList(photos);
+    shuffleList(photos);
+    // Adding Game Photo to Game Board
+    photos.forEach(function(photo) {
+      const gPhoto = photo;
+      gBoard.appendChild(document.createElement('div')).innerHTML = gPhoto;
+    });
   }
-  // Re-arranging Photos For Game Start
-  shuffleList(photos);
-  shuffleList(photos);
-  shuffleList(photos);
-  shuffleList(photos);
-  // Adding Game Photo to Game Board
-  photos.forEach(function(photo) {
-    const gPhoto = photo + 'id="wrong" onClick="wrongAnt()" ' + ' />';
-    gBoard.appendChild(document.createElement('div')).innerHTML = gPhoto;
-  });
 }
 
 // Game Timer Function
@@ -108,7 +146,7 @@ const gEngine = () => {
   var gTimer = 
   setInterval(function(){
     gTym -= 1;
-    gBoardCounter.innerText = gTym;
+    gBoardCounter.innerText = "Timer: " + gTym;
     if (gTym == 0){  // If Time is Over
       clearInterval(gTimer);
       confuse.play();
@@ -123,21 +161,22 @@ const gEngine = () => {
       clearInterval(gTimer);
       systemPoints += 3;
       levelPoint += 1;
-      glevel.innerText = "Lv " + level;
+      glevel.innerText = "Level: " + level;
       userCoins = 0;
+      userScore += 10;
       contiNue.play();
-      gBoardCoins.innerHTML = '<span>' + m25 + " " + userCoins + '</span>';
+      gBoardScore.innerHTML = "Score: " + userScore;
       gBoardCounter.innerText = 0;
       newLevel();
     }
     levelPoint;
-    if (levelPoint == 7) {
+    if (levelPoint == 5) {
       systemPoints = 10;
       clapping.play();
       level += 1;
       glevel.innerText = "Lv " + level;
       levelPoint = 0;
-      gamePlay.innerHTML = "<h2> Find this image and collect " + systemPoints + " coins </h2> <br/>" + imgList[12] + "/>";
+      gamePlay.innerHTML = "<h2> Find this image" + systemPoints + " coins </h2> <br/>" + imgList[12] + "/>";
       goal.innerText = "G-" + systemPoints;
       timeUp -= 10;
       apostle.play();
@@ -146,9 +185,14 @@ const gEngine = () => {
 }
 
 // Game Task Menu
-gamePlay.innerHTML = "<h2> Find this image and collect 10 coins </h2> <br/>" + imgList[12] + "/>";
-goal.innerText = "G-" + systemPoints;
-    
+if (gamePick == gameList[1]){
+  gamePlay.innerHTML = "<h2> Find this image </h2> <br/>" + imgList[12] + "/>";
+  goal.innerText = "G-" + systemPoints;
+}else {
+  gamePlay.innerHTML = "<h2> Find this number </h2> <br/> <div class='div'>" + numList[14] + "</div>";
+  goal.innerText = "G-" + systemPoints;
+}
+
 // Function to Empty Board and Start Game
 const shuffle = () => {
   gBoard.innerHTML = "";
@@ -166,19 +210,28 @@ const shuffleSound = () => {
 // Function For Wrong Attempt
 const wrongAnt = () => {
   funke.play();
-  gBoardCoins.innerHTML = '<span>' + m25 + " " + userCoins + '</span>';
+  gBoardScore.innerHTML = "Score: " + userScore;
 }
 
 // Function For Correct Attempt
 const antClick = (pic) => {
   pic.parentNode.remove();
   userCoins += 1;
-  gBoardCoins.innerHTML = '<span>' + m25 + " " + userCoins + '</span>';
+  userScore += 2;
+  gBoardScore.innerHTML = "Score: " + userScore;
   reshuffleMe += 1;
-  if (reshuffleMe == 4){
-    shuffleSound();
-    reshuffleMe = 0;
+  if (gamePick == gameList[1]) {
+    if (reshuffleMe >= 4){
+      shuffleSound();
+      reshuffleMe = 0;
+    }
+  }else {
+    if (reshuffleMe >= 1){
+      shuffleSound();
+      reshuffleMe = 0;
+    }
   }
+  correct.play();
 }
 
 // Function For Play Button
@@ -191,7 +244,15 @@ const play = () => {
   gBoard.style.display = "grid";
   gEngine();
   instrumental.play();
-  gImage.innerHTML = imgList[12] + "/>";
+  if (gamePick == gameList[1]) {
+    gImage.innerHTML = imgList[12] + "/>";
+    gImage.style.width = "10vw";
+    gImage.style.padding = "0vh";
+    gImage.style.margin = "0vh";
+  }else {
+    gImage.innerHTML = "Hint: " + numList[14];
+    gImage.style.padding = "2vh";
+  }
   sound.style.display = "block";
 }
 
@@ -200,18 +261,26 @@ const newLevel = () => {
   gBoard.style.display = "none";
   gamePlay.style.display = "block";
   gamePlay.style.paddingRight = "0vh";
-  shuffleList(imgList);
-  gamePlay.innerHTML = "<h2> Find this image and collect " + systemPoints + " coins </h2> <br/>" + imgList[12] + "/>";
+  // Select Which Board (Images or Numbers)
+  gamePick = gameList[Math.floor(Math.random() * gameList.length)];
+  if (gamePick == gameList[1]) {
+    shuffleList(imgList);
+    gamePlay.innerHTML = "<h2> Find this image</h2> <br/>" + imgList[12] + "/>";
+  }else {
+    shuffleList(numList);
+    gamePlay.innerHTML = "<h2> Find this number</h2> <br/> <div class='div'>" + numList[14] + "</div>";
+  }
   goal.innerText = "G-" + systemPoints;
   dashboard.style.paddingTop = "20vh";
   playButton.style.display = "block";
   shuffleButton.style.display = "none";
   sound.style.display = "none";
+  clapping.play();
 }
 
 // Game over Display When a User Loses
 const tryAgain = () => {
-  gamePlay.innerHTML = "<img src='img/gameover.gif' /> <div id='try' onClick ='yes()'> <br/> <h2> Would you like to play again <br/> <br/> <a href='game.html'><button id='yes'> Yes </button></a> <a href='index.html'><button id='no'> No </button></a> </div> <br/>";
+  gamePlay.innerHTML = "<h2> Your Score: " + userScore + "</h2> <br/><img src='img/gameover.gif' /> <div id='try' onClick ='yes()'> <br/> <h2> Would you like to play again <br/> <br/> <a href='game.html'><button id='yes'> Yes </button></a> <a href='index.html'><button id='no'> No </button></a> </div> <br/>";
   playButton.style.display = "none";
   gamePlay.style.paddingRight = "0vh";
 }
